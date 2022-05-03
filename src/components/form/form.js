02c0f31ecb1b83eb7e90bcc130562d1c98bfa229
Tanner253 <PercivalTanner@gmail.com>
2022-05-02 18:11:21 -0700

    adds

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,33 +1,49 @@
-import { Button, Icon } from "@blueprintjs/core";
+import React, { useState } from 'react';
+import { Button, Icon, FormGroup, InputGroup, Slider } from "@blueprintjs/core";
 
-export default function form(props){
-
-  return(
+export default function form(props) {
+  const [value, setValue] = useState(4)
+  return (
     <form onSubmit={props.handleSubmit}>
 
-    <h2>Add To Do Item</h2>
+      <h2>Add To Do Item</h2>
+      <FormGroup
+        inline={'true'}
+        helperText="enter task information"
+        label="To Do Item"
+        labelFor="item-name"
+        labelInfo="(required)"
+      >
+        <InputGroup onChange={props.handleChange} id="item-name" placeholder="To Do Item Details" />
+      </FormGroup>
+      <FormGroup
+        inline={true}
+        helperText="assign task to someone"
+        label="Assignee"
+        labelFor="assigned-name"
+        labelInfo="(required)"
+      >
+        <InputGroup onChange={props.handleChange} id="assigned-name" placeholder="To Do Item Details" />
+      </FormGroup>
+      <Slider
+                    min={0}
+                    max={5}
+                    stepSize={1}
+                    onChange={(value)=> {setValue(value)}}
+                    showTrackFill={true}
+                    value={value}
+                    vertical={'vertical'}
+                    name='difficulty' 
+                    id='difficulty'
+                />
 
-    <label>
-      <span>To Do Item</span>
-      <input onChange={props.handleChange} name="text" type="text" placeholder="Item Details" />
-    </label>
 
-    <label>
-      <span>Assigned To</span>
-      <input onChange={props.handleChange} name="assignee" type="text" placeholder="Assignee Name" />
-    </label>
-
-    <label>
-      <span>Difficulty</span>
-      <input onChange={props.handleChange} defaultValue={props.defaultValues.difficulty} type="range" min={1} max={5} name="difficulty" />
-    </label>
-
-    <label>
-      <Button icon='add' type="submit" intent="success" />
-    </label>
-  </form>
+      <label>
+        <Button icon='add' type="submit" intent="success" />
+      </label>
+    </form>
   )
-} 
+}
 
 
 
